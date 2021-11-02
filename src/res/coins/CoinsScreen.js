e97a3c692889639bf00169657fc4a7012c4108aa
Edgar <edgarrodriguezbello@gmail.com>
2021-11-02 17:46:32 +0100

    Añadida lista y loading

--- src/res/coins/CoinsScreen.js
+++ src/res/coins/CoinsScreen.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import {
+    View,
+    Text,
+    Pressable,
+    StyleSheet,
+    FlatList,
+    ActivityIndicator,
+} from "react-native";
+import Http from "../../libs/http";
+import CoinsItem from "./CoinsItem";
 
 function CoinsScreen({ navigation }) {
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const getData = async () => {
+        setLoading(true);
+        let coins = await Http.instance.get(
+            "https://api.coinlore.net/api/tickers/"
+        );
+        setData(coins.data);
+        setLoading(false);
+        console.log("coins", coins);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     const handlePress = () => {
         console.log("go to detail");
         navigation.navigate("CoinDetail");
@@ -9,10 +35,26 @@ function CoinsScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.titleText}>Coins Screen</Text>
+            {loading === true ? (
+                <ActivityIndicator size="large" animating={loading} />
+            ) : null}
+
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <CoinsItem item={item} />
+                    // <View>
+                    //     <Text>{item.name}</Text>
+                    //     <Text>{item.symbol}</Text>
+                    //     {/* <Text>{item.name}</Text> */}
+                    // </View>
+                )}
+            />
+            {/* <Text style={styles.titleText}>Coins Screen</Text>
             <Pressable onPress={handlePress} style={styles.btn}>
                 <Text style={styles.btnText}>Ir a detail</Text>
-            </Pressable>
+            </Pressable> */}
         </View>
     );
 }
